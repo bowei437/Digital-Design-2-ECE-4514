15a395b5d898730539976ca61b51f6280d69db40
bowei437 <bowei@vt.edu>
2017-03-02 14:38:19 -0500

    uyp

--- project5/myfpadd.v
+++ project5/myfpadd.v
@@ -14,7 +14,8 @@ module myfpadd(add_sub, clock, dataa, datab, result);
 	reg done;
 
 	reg [6:0] exp_a, exp_b, shiftamount;
-	reg [23:0] mant_a, mant_b, tmant_a, tmant_b, mant_comb, mant_comb2, mant_comb3;
+	reg [23:0] mant_a, mant_b, tmant_a, tmant_b;
+	reg [27:0] mant_comb;
 
 
 
@@ -27,17 +28,38 @@ module myfpadd(add_sub, clock, dataa, datab, result);
 				exp_b = exp_b + 1; // increment loop
 			end
 
-			if (dataa[31] == 1'b1) begin // if A is negative
+			if ((dataa[31] == 1'b1) && (datab[31] == 1'b0)) begin // if A is negative ONLY
 				mant_comb = mant_a - tmant_b;
-				tempresult = {dataa[31], exp_a[6:0], mant_comb};
+				while ((mant_comb[23] == 1'b0) && (mant_comb[22] == 1'b0) && (mant_comb[21] == 1'b0) && (mant_comb[20] == 1'b0)) begin
+					mant_comb = mant_comb << 4;
+					exp_a = exp_a - 1; 
+				end
+				tempresult = {dataa[31], exp_a[6:0], mant_comb[23:0]};
 			end
-			else if (datab[31] == 1'b1) begin // if B is negative
+			else if ((dataa[31] == 1'b0) && (datab[31] == 1'b1)) begin // if B is negative ONLY
 				mant_comb = mant_a - tmant_b;
-				tempresult = {dataa[31], exp_a[6:0], mant_comb};
+				while ((mant_comb[23] == 1'b0) && (mant_comb[22] == 1'b0) && (mant_comb[21] == 1'b0) && (mant_comb[20] == 1'b0)) begin
+					mant_comb = mant_comb << 4;
+					exp_a = exp_a - 1; 
+				end
+				tempresult = {dataa[31], exp_a[6:0], mant_comb[23:0]};
+			end
+			else if ((dataa[31] == 1'b1) && (datab[31] == 1'b1)) begin // BOTH NEGATIVE
+				mant_comb = mant_a + tmant_b;
+				while ((mant_comb[23] == 1'b0) && (mant_comb[22] == 1'b0) && (mant_comb[21] == 1'b0) && (mant_comb[20] == 1'b0)) begin
+					mant_comb = mant_comb << 4;
+					//exp_a = exp_a - 1; 
+				end
+				tempresult = {dataa[31], exp_a[6:0], mant_comb[23:0]};
 			end
 			else begin
 				mant_comb = mant_a + tmant_b;
-				tempresult = {dataa[31], exp_a[6:0], mant_comb};
+				while (mant_comb[27:24] != 4'b0) begin
+					mant_comb = mant_comb >> 4;
+					exp_a = exp_a + 1;
+				end
+
+				tempresult = {dataa[31], exp_a[6:0], mant_comb[23:0]};
 			end
 			done = 1'b1;
 				//exp_a = 7'b0;
@@ -52,16 +74,20 @@ module myfpadd(add_sub, clock, dataa, datab, result);
 			end
 
 			if (dataa[31] == 1'b1) begin
-				mant_comb2 = mant_b - tmant_a;
-				tempresult = {datab[31], exp_b[6:0], mant_comb2};
+				mant_comb = mant_b - tmant_a;
+				while ((mant_comb[23] == 1'b0) && (mant_comb[22] == 1'b0) && (mant_comb[21] == 1'b0) && (mant_comb[20] == 1'b0)) begin
+					mant_comb = mant_comb << 4;
+					exp_b = exp_b - 1; 
+				end
+				tempresult = {datab[31], exp_b[6:0], mant_comb[23:0]};
 			end
 			else if (datab[31] == 1'b1) begin
-				mant_comb2 = mant_b - tmant_a;
-				tempresult = {datab[31], exp_b[6:0], mant_comb2};
+				mant_comb = mant_b - tmant_a;
+				tempresult = {datab[31], exp_b[6:0], mant_comb[23:0]};
 			end
 			else begin
-				mant_comb2 = tmant_a + mant_b;
-				tempresult = {datab[31], exp_b[6:0], mant_comb2};
+				mant_comb = tmant_a + mant_b;
+				tempresult = {datab[31], exp_b[6:0], mant_comb[23:0]};
 			end
 			done = 1'b1;
 				//exp_a = 7'b0;
@@ -69,16 +95,16 @@ module myfpadd(add_sub, clock, dataa, datab, result);
 		end
 		else if (exp_a == exp_b) begin
 			if (dataa[31] == 1'b1) begin
-				mant_comb3 = mant_a - mant_b;
-				tempresult = {dataa[31], exp_a[6:0], mant_comb3};
+				mant_comb = mant_b - mant_a;
+				tempresult = {datab[31], exp_a[6:0], mant_comb[23:0]};
 			end
 			else if (datab[31] == 1'b1) begin
-				mant_comb3 = mant_a - mant_b;
-				tempresult = {dataa[31], exp_a[6:0], mant_comb3};
+				mant_comb = mant_a - mant_b;
+				tempresult = {datab[31], exp_a[6:0], mant_comb[23:0]};
 			end
 			else begin
-				mant_comb3 = mant_a + mant_b;
-				tempresult = {dataa[31], exp_a[6:0], mant_comb3};
+				mant_comb = mant_a + mant_b;
+				tempresult = {datab[31], exp_a[6:0], mant_comb[23:0]};
 			end
 			
 			done = 1'b1;
