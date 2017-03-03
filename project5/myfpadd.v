7810302907efce6c793d9e04d1fffa885338c13a
bowei437 <bowei@vt.edu>
2017-03-02 21:13:01 -0500

    working tested with sinc

--- project5/myfpadd.v
+++ project5/myfpadd.v
@@ -21,8 +21,9 @@ module myfpadd(add_sub, clock, dataa, datab, result);
 
    always @* begin
    		done = 1'b0;
+		///////////if Input exponent A > B  /////////////////
 		if (exp_a > exp_b) begin
-
+			// Normalizes exponents by shifting left by 4 for every 1 value difference
 			while (exp_a > exp_b) begin
 				tmant_b = mant_b >> 4; // shift right by 4
 				exp_b = exp_b + 1; // increment loop
@@ -44,7 +45,7 @@ module myfpadd(add_sub, clock, dataa, datab, result);
 				end
 				tempresult = {dataa[31], exp_a[6:0], mant_comb[23:0]};
 			end
-			else if ((dataa[31] == 1'b1) && (datab[31] == 1'b1)) begin // BOTH NEGATIVE
+			else if ((dataa[31] == 1'b1) && (datab[31] == 1'b1)) begin // IF A and B BOTH NEGATIVE
 				mant_comb = mant_a + tmant_b;
 				while ((mant_comb[23] == 1'b0) && (mant_comb[22] == 1'b0) && (mant_comb[21] == 1'b0) && (mant_comb[20] == 1'b0)) begin
 					mant_comb = mant_comb << 4;
@@ -66,18 +67,24 @@ module myfpadd(add_sub, clock, dataa, datab, result);
 				//exp_b = 7'b0;
 			
 		end
+		///////////if Input exponent B > A  /////////////////
 		else if (exp_b > exp_a) begin // When exponent of B greater than A
 
+			// Normalizes exponents by shifting left by 4 for every 1 value difference
 			while (exp_b > exp_a) begin
 				tmant_a = mant_a >> 4; // shift A to right by 4 to normalize
 				exp_a = exp_a + 1; // increment so it loops
 			end
 
 			if ((dataa[31] == 1'b1) && (datab[31] == 1'b0)) begin // if A is negative ONLY
+				// can subtract B from A to get the same result as addition
 				mant_comb = mant_b - tmant_a;
+				// While loop is to check if result of "addition" is still normalized or if the exponents shifted
+
 				while ((mant_comb[23] == 1'b0) && (mant_comb[22] == 1'b0) && (mant_comb[21] == 1'b0) && (mant_comb[20] == 1'b0)) begin
+					// if it is not normalized. We shift it left 4 until those 4 arrays are no longer NON zero
 					mant_comb = mant_comb << 4;
-					exp_b = exp_b - 1; 
+					exp_b = exp_b - 1; // subtract the base exponent for the result output
 				end
 				tempresult = {datab[31], exp_b[6:0], mant_comb[23:0]};
 			end
@@ -89,7 +96,7 @@ module myfpadd(add_sub, clock, dataa, datab, result);
 				end
 				tempresult = {datab[31], exp_b[6:0], mant_comb[23:0]};
 			end
-			else if ((dataa[31] == 1'b1) && (datab[31] == 1'b1)) begin /// BOTH NEGATIVE
+			else if ((dataa[31] == 1'b1) && (datab[31] == 1'b1)) begin /// IF A and B BOTH NEGATIVE
 				mant_comb = tmant_a + mant_b;
 				while ((mant_comb[23] == 1'b0) && (mant_comb[22] == 1'b0) && (mant_comb[21] == 1'b0) && (mant_comb[20] == 1'b0)) begin
 					mant_comb = mant_comb << 4;
@@ -109,14 +116,23 @@ module myfpadd(add_sub, clock, dataa, datab, result);
 				//exp_a = 7'b0;
 				//exp_b = 7'b0;
 		end
+		///////////if Input exponent A == B  /////////////////
 		else if (exp_a == exp_b) begin
 			if ((dataa[31] == 1'b1) && (datab[31] == 1'b0)) begin // if A is negative ONLY
 				if (mant_a > mant_b) begin
 					mant_comb = mant_a - mant_b;
+					while ((mant_comb[23] == 1'b0) && (mant_comb[22] == 1'b0) && (mant_comb[21] == 1'b0) && (mant_comb[20] == 1'b0)) begin
+						mant_comb = mant_comb << 4;
+						exp_a = exp_a - 1; 
+					end
 					tempresult = {dataa[31], exp_a[6:0], mant_comb[23:0]};
 				end
 				else begin
 					mant_comb = mant_b - mant_a;
+					while ((mant_comb[23] == 1'b0) && (mant_comb[22] == 1'b0) && (mant_comb[21] == 1'b0) && (mant_comb[20] == 1'b0)) begin
+						mant_comb = mant_comb << 4;
+						exp_a = exp_a - 1; 
+					end
 					tempresult = {datab[31], exp_a[6:0], mant_comb[23:0]};
 				end
 				
@@ -124,15 +140,23 @@ module myfpadd(add_sub, clock, dataa, datab, result);
 			else if ((dataa[31] == 1'b0) && (datab[31] == 1'b1)) begin // if B is negative ONLY
 				if (mant_a > mant_b) begin
 					mant_comb = mant_a - mant_b;
+					while ((mant_comb[23] == 1'b0) && (mant_comb[22] == 1'b0) && (mant_comb[21] == 1'b0) && (mant_comb[20] == 1'b0)) begin
+						mant_comb = mant_comb << 4;
+						exp_a = exp_a - 1; 
+					end
 					tempresult = {dataa[31], exp_a[6:0], mant_comb[23:0]};
 				end
 				else begin
 					mant_comb = mant_b - mant_a;
+					while ((mant_comb[23] == 1'b0) && (mant_comb[22] == 1'b0) && (mant_comb[21] == 1'b0) && (mant_comb[20] == 1'b0)) begin
+						mant_comb = mant_comb << 4;
+						exp_a = exp_a - 1; 
+					end
 					tempresult = {datab[31], exp_a[6:0], mant_comb[23:0]};
 				end
 
 			end
-			else if ((dataa[31] == 1'b1) && (datab[31] == 1'b1)) begin // both negative
+			else if ((dataa[31] == 1'b1) && (datab[31] == 1'b1)) begin // IF A and B BOTH NEGATIVE
 				mant_comb = mant_a + mant_b;
 				while (mant_comb[27:24] != 4'b0) begin
 					mant_comb = mant_comb >> 4;
--- project5/tb_fsm.v
+++ project5/tb_fsm.v
@@ -21,70 +21,45 @@ module tb_fsm();
         dataa = 32'H0129126E; // 2.567
         datab = 32'H01520831; // 5.127 
         // result should equal 7.694 or 017B1A9F
+
+
         #60;
-        dataa = 32'H82130000; // -19
-        datab = 32'H01A00000; //  10
-        // result =  9 or 01900000
+        dataa = 32'H01100000; //  1
+        datab = 32'H812AAAAA; //  -2.666667
+        // result =  or 
         #60;
-        dataa = 32'H02120000; // 18
-        datab = 32'H81F00000; //  -15
-        // result =  or 01300000
+        dataa = 32'H01222222; // 2.1333
+        datab = 32'H80D00D00; //  -0.81269
+        // result =  or 
         #60;
-        dataa = 32'H82130000; // -19
-        datab = 32'H81E00000; //  -14
-        // result =  or 82210000
+        dataa = 32'H01152152; // 1.32063
+        datab = 32'H002E3BC7; //  0.180599
+        // result =  or 
         #60;
-        dataa = 32'H02FF0000; // 255
-        datab = 32'H01F00000; //  15
-        // result =  or 0310E000
+        dataa = 32'H811AAAAA; // -1.666667
+        datab = 32'H0118050E; //  1.50123
+        // result =  or 
         #60;
-        dataa = 32'H01B00000; // 11
-        datab = 32'H82120000; //  -18
-        // result =  or 81700000
+        dataa = 32'H811AAACD; // -1.666667
+        datab = 32'H011804EA; //  1.50123
+        // result =  or 
         #60;
-        dataa = 32'H81D00000; // -13
-        datab = 32'H02110000; //  17
-        // result =  or 01400000
+        dataa = 32'H00000000; // 
+        datab = 32'H00000000; //  
+        // result =  or 
         #60;
-        dataa = 32'H81A00000; // -10
-        datab = 32'H82190000; //  -25
-        // result =  or 82230000
+        dataa = 32'H00000000; // 
+        datab = 32'H00000000; //  
+        // result =  or 
         #60;
-        dataa = 32'H01F00000; // 15
-        datab = 32'H02FF0000; //  255
-        // result =  or 0310E000
+        dataa = 32'H00000000; // 
+        datab = 32'H00000000; //  
+        // result =  or 
         #60;
-        dataa = 32'H01F00000; // 15
-        datab = 32'H01F00000; //  15
-        // result =  or 021E0000
-        #60;
-        dataa = 32'H81E00000; // -14
-        datab = 32'H01C00000; //  12
-        // result =  or 81200000
-        #60;
-        dataa = 32'H81500000; // -5
-        datab = 32'H01B00000; //  11
-        // result =  or 01600000
-        #60;
-        dataa = 32'H01F00000; // 15
-        datab = 32'H81C00000; //  -12
-        // result =  or 01300000
-        #60;
-        dataa = 32'H81E00000; // -14
-        datab = 32'H81B00000; //  -11
-        // result =  or 82190000
-        #60;
-        dataa = 32'H81500000; // -5
-        datab = 32'H81500000; //  -5
-        // result =  or 81A00000
-        #60;
-        dataa = 32'H01400000; // 4
-        datab = 32'H01800000; //  8
-        // result =  or 01C00000
-        #60;
-        dataa = 32'H01900000; // 9
-        datab = 32'H01A00000; //  10
-        // result =  or 02130000
+        dataa = 32'H00000000; // 
+        datab = 32'H00000000; //  
+        // result =  or 
+
         #80;
             //dataa = 32'H018E3D46; //8.88996
         //dataa = 32'H01FE58E2; // 15.8967
@@ -152,4 +127,70 @@ endmodule
         dataa = 32'H01B00000; // 11
         datab = 32'H81200000; // -2
         // result = 9 or 01900000
+        */
+
+        /* TEST 2
+        dataa = 32'H82130000; // -19
+        datab = 32'H01A00000; //  10
+        // result =  9 or 01900000
+        #60;
+        dataa = 32'H02120000; // 18
+        datab = 32'H81F00000; //  -15
+        // result =  or 01300000
+        #60;
+        dataa = 32'H82130000; // -19
+        datab = 32'H81E00000; //  -14
+        // result =  or 82210000
+        #60;
+        dataa = 32'H02FF0000; // 255
+        datab = 32'H01F00000; //  15
+        // result =  or 0310E000
+        #60;
+        dataa = 32'H01B00000; // 11
+        datab = 32'H82120000; //  -18
+        // result =  or 81700000
+        #60;
+        dataa = 32'H81D00000; // -13
+        datab = 32'H02110000; //  17
+        // result =  or 01400000
+        #60;
+        dataa = 32'H81A00000; // -10
+        datab = 32'H82190000; //  -25
+        // result =  or 82230000
+        #60;
+        dataa = 32'H01F00000; // 15
+        datab = 32'H02FF0000; //  255
+        // result =  or 0310E000
+        #60;
+        dataa = 32'H01F00000; // 15
+        datab = 32'H01F00000; //  15
+        // result =  or 021E0000
+        #60;
+        dataa = 32'H81E00000; // -14
+        datab = 32'H01C00000; //  12
+        // result =  or 81200000
+        #60;
+        dataa = 32'H81500000; // -5
+        datab = 32'H01B00000; //  11
+        // result =  or 01600000
+        #60;
+        dataa = 32'H01F00000; // 15
+        datab = 32'H81C00000; //  -12
+        // result =  or 01300000
+        #60;
+        dataa = 32'H81E00000; // -14
+        datab = 32'H81B00000; //  -11
+        // result =  or 82190000
+        #60;
+        dataa = 32'H81500000; // -5
+        datab = 32'H81500000; //  -5
+        // result =  or 81A00000
+        #60;
+        dataa = 32'H01400000; // 4
+        datab = 32'H01800000; //  8
+        // result =  or 01C00000
+        #60;
+        dataa = 32'H01900000; // 9
+        datab = 32'H01A00000; //  10
+        // result =  or 02130000
         */
